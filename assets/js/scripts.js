3129974297ce00644294a84f572e15f4444e45a1
Mario <mariob360@gmail.com>
2017-09-12 19:37:03 -0700

    added delete functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,14 @@
 // Check off the "todos" by clicking on them
 
-$("li").on("click", function () {
-  $(this).toggleClass("completed")
+$("li").on("click", function() {
+  $(this).toggleClass("completed");
 });
+
+// Click on X to delete the "todo"
+
+$("span").on("click", function(e) {
+  e.stopPropagation();
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  })
+})
