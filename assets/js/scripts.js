8b20b45bf5e45e873a43ec13fa8916b40ab2e818
Mario <mariob360@gmail.com>
2017-09-12 19:12:24 -0700

    css approach to marking list items

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,5 @@
 // Check off the "todos" by clicking on them
 
 $("li").on("click", function () {
-  if ($(this).css("color") === "rgb(128, 128, 128)") {
-    $(this).css({
-      "color": "black",
-      "text-decoration": "none"
-    });
-    console.log("are you getting here")
-  } else {
-    $(this).css({
-      "color": "gray",
-      "text-decoration": "line-through"
-    });
-  }
+  $(this).toggleClass("completed")
 });
