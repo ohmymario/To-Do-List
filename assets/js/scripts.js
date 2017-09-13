7387414b7e8fbd11944edaa44e855721f0f203e5
Mario <mariob360@gmail.com>
2017-09-12 20:13:52 -0700

    user can now add additional list items

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,4 +11,15 @@ $("span").on("click", function(e) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   })
-})
+});
+
+$("input[type=text]").on("keypress", function(e) {
+  if(e.which === 13){
+    //grabbing user input for new todo list item
+    var todoText = $(this).val();
+    //clear to allow for additional input
+    $(this).val("");
+    // create a new li "todoText" and append to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
+});
